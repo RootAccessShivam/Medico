197a0d9ce97d256ca1de287c4c51b3f68de75dc4
Shivam Yadav <shivamyadav3516mum@gmail.com>
2026-04-26 18:34:24 +0530

    Ensure superuser creation in build

--- build.sh
+++ build.sh
@@ -1,4 +1,20 @@
 #!/usr/bin/env bash
+set -e
+
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
 python manage.py migrate
+
+# create superuser if not exists
+echo "from django.contrib.auth import get_user_model
+User = get_user_model()
+username='admin'
+password='admin123'
+email='admin@gmail.com'
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print('SUPERUSER_CREATED')
+else:
+    print('SUPERUSER_EXISTS')
+" | python manage.py shell
+
+python manage.py collectstatic --noinput
